979680cf6fcce1343ae5dbadadbe81b997c59566
diogommartins <diogo.martins@unirio.br>
2016-05-31 15:05:21 -0300

    Primeiro commit

---
+++ README.rst
@@ -0,0 +1,13 @@
+Web2py SIE DAO
+==============
+
+Python DAO module for **SIE** (Sistema de Informações para o Ensino)
+academic administration solution.
+
+*(This readme is in progress)*
+
+This module requires the following pip packages:
+
+-  deprecate
+-  enum (will be used in the future)
+-  pyDAL (will be removed in the future)
---
+++ setup.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+from os import path
+
+VERSION = '0.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='unirio-sie',
+    version=VERSION,
+    packages=find_packages(exclude=['*test*']),
+    description='Python DAO module for SIE(Sistema de Informações para o Ensino) academic administration solution.',
+    long_description=README,
+    url='https://github.com/unirio-dtic/unirio-sie',
+    author='Diogo Magalhães Martins',
+    author_email='magalhaesmartins@icloud.com',
+    license='GPLv2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    keywords='unirio api sie development',
+    install_requires=required
+)
+
+# todo: pre-publish -> pandoc --from=markdown --to=rst --output=README.rst README.md
