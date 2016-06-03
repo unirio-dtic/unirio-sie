9cfaa86ac0b1f2d5cbd4cef66fd95a85c2347027
diogommartins <diogo.martins@unirio.br>
2016-06-03 15:44:05 -0300

    Adicioada inteface básica de um dao

--- unirio/sie/base.py
+++ unirio/sie/base.py
@@ -18,3 +18,32 @@ class SIE(object):
     def handle_blob(self, arquivo):
         # TODO descarta @staticmethod...
         return self.__adapter.handle_blob(arquivo)
+
+
+class BaseDAO(SIE):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractproperty
+    def path(self):
+        pass
+
+    def get(self, **kwargs):
+        """
+        :param kwargs:
+        :return:
+        """
+        if 'uid' in kwargs:
+            return self.api.get_single_result(self.path, kwargs['uid'])
+        raise NotImplementedError
+
+    def search(self, *args, **kwargs):
+        return self.api.get(self.path, *args, **kwargs)
+
+    def delete(self, uid):
+        return self.api.delete(uid)
+
+    def insert(self, data):
+        return self.api.post(self.path, data)
+
+    def update(self, data):
+        return self.api.put(self.path, data)
